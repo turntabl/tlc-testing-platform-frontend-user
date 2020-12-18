f1ff4653c3138348a2d37771b9eb7cea5c4c0f78
humaidu-ali <humaidu.ali@turntabl.io>
2020-12-17 23:34:42 -0800

    view results and review test pages created

--- src/app/pages/review-test/review-test.component.ts
+++ src/app/pages/review-test/review-test.component.ts
@@ -1,15 +1,35 @@
 import { Component, OnInit } from '@angular/core';
+interface Result {
+  course: string;
+  date: string;
+}
 
+const RESULTS: Result[] = [
+  {
+    course: 'Java',
+
+    date: '11/12/2020',
+  },
+  {
+    course: 'Python',
+
+    date: '20/12/2020',
+  },
+  {
+    course: 'Scala',
+
+    date: '30/12/2020',
+  },
+];
 @Component({
   selector: 'app-review-test',
   templateUrl: './review-test.component.html',
-  styleUrls: ['./review-test.component.css']
+  styleUrls: ['./review-test.component.css'],
 })
 export class ReviewTestComponent implements OnInit {
+  results = RESULTS;
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
--- src/app/pages/view-result/view-result.component.ts
+++ src/app/pages/view-result/view-result.component.ts
@@ -1,15 +1,41 @@
 import { Component, OnInit } from '@angular/core';
 
+interface Result {
+  course: string;
+  score: string;
+  grade: string;
+  date: string;
+}
+
+const RESULTS: Result[] = [
+  {
+    course: 'Java',
+    score: '87%',
+    grade: 'A',
+    date: '11/12/2020',
+  },
+  {
+    course: 'Python',
+    score: '75%',
+    grade: 'B',
+    date: '20/12/2020',
+  },
+  {
+    course: 'Scala',
+    score: '35%',
+    grade: 'Failed',
+    date: '30/12/2020',
+  },
+];
 @Component({
   selector: 'app-view-result',
   templateUrl: './view-result.component.html',
-  styleUrls: ['./view-result.component.css']
+  styleUrls: ['./view-result.component.css'],
 })
 export class ViewResultComponent implements OnInit {
+  results = RESULTS;
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
