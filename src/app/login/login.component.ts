9573ee05240b4d1936bcb2c532d776d1858c20ea
despitegfx <despitegfx@gmail.com>
2021-01-01 01:35:12 +0000

    login added

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { GoogleLoginProvider, SocialAuthService, SocialUser } from 'angularx-social-login';
+import { LocalDataService } from '../service/local-data.service';
+import { LoginService } from '../service/login.service';
 
 @Component({
   selector: 'app-login',
@@ -6,13 +10,32 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent implements OnInit {
+  user: SocialUser;
 
-  constructor() { }
+  constructor(private loginService: LoginService, private authService: SocialAuthService, private router: Router) { 
+    this.user = new SocialUser;
+  }
 
   ngOnInit(): void {
-  }
-  signInWithGoogle(){
-    
+    this.checkLogin();
   }
 
+  checkLogin() {
+    if (localStorage.getItem('id') != null) {
+      this.router.navigate(['/user-dashboard']);
+    }
+  }
+
+  signInWithGoogle(): void {
+      this.authService.signIn(GoogleLoginProvider.PROVIDER_ID);
+      this.authService.authState.subscribe(user => {
+        this.user = user;
+        var getData = {
+          email: this.user.email,
+          photo: this.user.photoUrl,
+        };
+        localStorage.setItem('id', JSON.stringify(getData));
+        this.checkLogin();
+      });
+  }
 }
--- src/app/pages/user-profile/user-profile.component.ts
+++ src/app/pages/user-profile/user-profile.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { LoginService } from 'src/app/service/login.service';
 
 @Component({
   selector: 'app-user-profile',
@@ -7,7 +8,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class UserProfileComponent implements OnInit {
 
-  constructor() { }
+  constructor(public loginService: LoginService) { }
 
   ngOnInit(): void {
   }
