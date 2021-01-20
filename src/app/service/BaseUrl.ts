944e4b25a65fc5819c8f360d9cb43be16fedce2e
Nicholas-TurnTabl <nicholas.dickson@turntabl.io>
2021-01-20 15:50:36 +0000

    Update BaseUrl.ts

--- src/app/service/BaseUrl.ts
+++ src/app/service/BaseUrl.ts
@@ -1,4 +1,4 @@
 export class BaseUrl{
-    baseURL:string= "ec2-18-194-33-225.eu-central-1.compute.amazonaws.com:8081";
+    baseURL:string= "http://ec2-18-194-33-225.eu-central-1.compute.amazonaws.com:8081";
     constructor(){};
 }
